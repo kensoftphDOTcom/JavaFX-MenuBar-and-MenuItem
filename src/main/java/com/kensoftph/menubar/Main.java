161ee25544b8f270a956e2c518e4fb22b50110c9
Kensoft PH <kensoftph.com@gmail.com>
2022-12-16 11:19:52 +0800

    new update

--- src/main/java/com/kensoftph/menubar/Main.java
+++ src/main/java/com/kensoftph/menubar/Main.java
@@ -3,10 +3,7 @@ package com.kensoftph.menubar;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -35,6 +32,43 @@ public class Main extends Application {
         MenuItem item2 = new MenuItem("Save");
         MenuItem item3 = new MenuItem("Exit");
 
+        // this RadioMenuItem
+        RadioMenuItem r1 = new RadioMenuItem("True");
+        RadioMenuItem r2 = new RadioMenuItem("False");
+        ToggleGroup rGroup = new ToggleGroup();
+        rGroup.getToggles().addAll(r1, r2);
+
+        r1.setOnAction(event -> {
+            alert();
+        });
+        r2.setOnAction(event -> {
+            alert();
+        });
+
+        // This one is CheckMenuItem
+        CheckMenuItem c1 = new CheckMenuItem("Stroke for Circle");
+        CheckMenuItem c2 = new CheckMenuItem("Stroke for Rectangle");
+        c1.setOnAction(event -> {
+            alert();
+        });
+        c2.setOnAction(event -> {
+            alert();
+        });
+
+        // This one submenu item
+        Menu submenu = new Menu("Save As");
+        submenu.getItems().addAll(r1, r2);
+
+        // Custom Menu Item
+        CustomMenuItem customItem = new CustomMenuItem();
+        Button btn = new Button("Kensoft PH");
+        customItem.setHideOnClick(false);
+        customItem.setContent(btn);
+
+        // Separator Menuitem
+        SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+        SeparatorMenuItem separatorMenuItem2 = new SeparatorMenuItem();
+
         item1.setOnAction(event -> {
             alert();
         });
@@ -45,7 +79,8 @@ public class Main extends Application {
             alert();
         });
 
-        file.getItems().addAll(item1, item2, item3);
+        file.getItems().addAll(item1, item2, submenu, separatorMenuItem, item3, separatorMenuItem2, c1, c2);
+        about.getItems().add(customItem);
 
         Scene scene = new Scene(layout, 500, 500);
         stage.setScene(scene);
